570662eb86120341ab858bf923a9efde4665714d
Brian Reavis <brian@thirdroute.com>
2013-06-06 21:46:30 -0700

    Updated selectize.js to latest version.

--- js/selectize.js
+++ js/selectize.js
@@ -1,6 +1,6 @@
-/*! selectize.js - v0.1.11 | https://github.com/brianreavis/selectize.js | Apache License (v2) */
+/*! selectize.js - v0.2.0 | https://github.com/brianreavis/selectize.js | Apache License (v2) */
 
-(function (factory) {
+(function(factory) {
 	if (typeof exports === 'object') {
 		factory(require('jquery'));
 	} else if (typeof define === 'function' && define.amd) {
@@ -109,115 +109,6 @@
 		'y': '[yŸÿý]',
 		'z': '[zŽž]'
 	};
-	
-	// --- src/selectize.jquery.js ---
-	
-	var defaults = {
-		delimiter: ',',
-		persist: true,
-		diacritics: true,
-		create: false,
-		highlight: true,
-		openOnFocus: true,
-		maxOptions: 1000,
-		maxItems: null,
-		hideSelected: null,
-	
-		scrollDuration: 60,
-		loadThrottle: 300,
-	
-		dataAttr: 'data-data',
-		sortField: null,
-		sortDirection: 'asc',
-		valueField: 'value',
-		labelField: 'text',
-		searchField: ['text'],
-	
-		mode: null,
-		theme: 'default',
-		wrapperClass: 'selectize-control',
-		inputClass: 'selectize-input',
-		dropdownClass: 'selectize-dropdown',
-	
-		load            : null, // function(query, callback)
-		score           : null, // function(search)
-		onChange        : null, // function(value)
-		onItemAdd       : null, // function(value, $item) { ... }
-		onItemRemove    : null, // function(value) { ... }
-		onClear         : null, // function() { ... }
-		onOptionAdd     : null, // function(value, data) { ... }
-		onOptionRemove  : null, // function(value) { ... }
-		onDropdownOpen  : null, // function($dropdown) { ... }
-		onDropdownClose : null, // function($dropdown) { ... }
-		onType          : null, // function(str) { ... }
-	
-		render: {
-			item: null,
-			option: null,
-			option_create: null
-		}
-	};
-	
-	$.fn.selectize = function (settings) {
-		var defaults = $.fn.selectize.defaults;
-		settings = settings || {};
-	
-		return this.each(function() {
-			var instance, value, values, i, n, data, dataAttr, settings_element, tagName;
-			var $options, $option, $input = $(this);
-	
-			tagName = $input[0].tagName.toLowerCase();
-	
-			if (typeof settings === 'string') {
-				instance = $input.data('selectize');
-				instance[settings].apply(instance, Array.prototype.splice.apply(arguments, 1));
-			} else {
-				dataAttr = settings.dataAttr || defaults.dataAttr;
-				settings_element = {};
-				settings_element.placeholder = $input.attr('placeholder');
-				settings_element.options = {};
-				settings_element.items = [];
-	
-				if (tagName === 'select') {
-					settings_element.maxItems = !!$input.attr('multiple') ? null : 1;
-					$options = $input.children();
-					for (i = 0, n = $options.length; i < n; i++) {
-						$option = $($options[i]);
-						value = $option.attr('value') || '';
-						if (!value.length) continue;
-						data = (dataAttr && $option.attr(dataAttr)) || {
-							'text'  : $option.html(),
-							'value' : value
-						};
-	
-						if (typeof data === 'string') data = JSON.parse(data);
-						settings_element.options[value] = data;
-						if ($option.is(':selected')) {
-							settings_element.items.push(value);
-						}
-					}
-				} else {
-					value = $.trim($input.val() || '');
-					if (value.length) {
-						values = value.split(settings.delimiter || defaults.delimiter);
-						for (i = 0, n = values.length; i < n; i++) {
-							settings_element.options[values[i]] = {
-								'text'  : values[i],
-								'value' : values[i]
-							};
-						}
-						settings_element.items = values;
-					}
-				}
-	
-				instance = new Selectize($input, $.extend(true, {}, defaults, settings_element, settings));
-				$input.data('selectize', instance);
-				$input.addClass('selectized');
-			}
-		});
-	};
-	
-	$.fn.selectize.defaults = defaults;
 	
 	// --- src/utils.js ---
 	
@@ -596,6 +487,11 @@
 		this.refreshItems();
 		this.updatePlaceholder();
 		this.isSetup = true;
+	
+		// preload options
+		if (this.settings.preload) {
+			this.onSearchChange('');
+		}
 	};
 	
 	/**
@@ -1880,6 +1776,116 @@
 	
 		return html;
 	};
+	
+	Selectize.defaults = {
+		delimiter: ',',
+		persist: true,
+		diacritics: true,
+		create: false,
+		highlight: true,
+		openOnFocus: true,
+		maxOptions: 1000,
+		maxItems: null,
+		hideSelected: null,
+		preload: false,
+	
+		scrollDuration: 60,
+		loadThrottle: 300,
+	
+		dataAttr: 'data-data',
+		sortField: null,
+		sortDirection: 'asc',
+		valueField: 'value',
+		labelField: 'text',
+		searchField: ['text'],
+	
+		mode: null,
+		theme: 'default',
+		wrapperClass: 'selectize-control',
+		inputClass: 'selectize-input',
+		dropdownClass: 'selectize-dropdown',
+	
+		load            : null, // function(query, callback)
+		score           : null, // function(search)
+		onChange        : null, // function(value)
+		onItemAdd       : null, // function(value, $item) { ... }
+		onItemRemove    : null, // function(value) { ... }
+		onClear         : null, // function() { ... }
+		onOptionAdd     : null, // function(value, data) { ... }
+		onOptionRemove  : null, // function(value) { ... }
+		onDropdownOpen  : null, // function($dropdown) { ... }
+		onDropdownClose : null, // function($dropdown) { ... }
+		onType          : null, // function(str) { ... }
+	
+		render: {
+			item: null,
+			option: null,
+			option_create: null
+		}
+	};
+	
+	// --- src/selectize.jquery.js ---
+	
+	$.fn.selectize = function(settings) {
+		var defaults = $.fn.selectize.defaults;
+		settings = settings || {};
+	
+		return this.each(function() {
+			var instance, value, values, i, n, data, dataAttr, settings_element, tagName;
+			var $options, $option, $input = $(this);
+	
+			tagName = $input[0].tagName.toLowerCase();
+	
+			if (typeof settings === 'string') {
+				instance = $input.data('selectize');
+				instance[settings].apply(instance, Array.prototype.splice.apply(arguments, 1));
+			} else {
+				dataAttr = settings.dataAttr || defaults.dataAttr;
+				settings_element = {};
+				settings_element.placeholder = $input.attr('placeholder');
+				settings_element.options = {};
+				settings_element.items = [];
+	
+				if (tagName === 'select') {
+					settings_element.maxItems = !!$input.attr('multiple') ? null : 1;
+					$options = $input.children();
+					for (i = 0, n = $options.length; i < n; i++) {
+						$option = $($options[i]);
+						value = $option.attr('value') || '';
+						if (!value.length) continue;
+						data = (dataAttr && $option.attr(dataAttr)) || {
+							'text'  : $option.html(),
+							'value' : value
+						};
+	
+						if (typeof data === 'string') data = JSON.parse(data);
+						settings_element.options[value] = data;
+						if ($option.is(':selected')) {
+							settings_element.items.push(value);
+						}
+					}
+				} else {
+					value = $.trim($input.val() || '');
+					if (value.length) {
+						values = value.split(settings.delimiter || defaults.delimiter);
+						for (i = 0, n = values.length; i < n; i++) {
+							settings_element.options[values[i]] = {
+								'text'  : values[i],
+								'value' : values[i]
+							};
+						}
+						settings_element.items = values;
+					}
+				}
+	
+				instance = new Selectize($input, $.extend(true, {}, defaults, settings_element, settings));
+				$input.data('selectize', instance);
+				$input.addClass('selectized');
+			}
+		});
+	};
+	
+	$.fn.selectize.defaults = Selectize.defaults;
 
 	return Selectize;
 
