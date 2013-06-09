785310601916a347027ee87fe0a8073870af5a6c
Brian Reavis <brian@thirdroute.com>
2013-06-08 18:42:50 -0700

    Released 0.2.3.

--- selectize.js
+++ selectize.js
@@ -1,4 +1,4 @@
-/*! selectize.js - v0.2.2 | https://github.com/brianreavis/selectize.js | Apache License (v2) */
+/*! selectize.js - v0.2.3 | https://github.com/brianreavis/selectize.js | Apache License (v2) */
 
 (function(factory) {
 	if (typeof exports === 'object') {
