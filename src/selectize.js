48430d8942b46e09ebd738c41d40dff94c156af8
Brian Reavis <brian@thirdroute.com>
2013-09-02 08:31:18 -0700

    Fixed `this` context on onDelete callback.

--- src/selectize.js
+++ src/selectize.js
@@ -1429,7 +1429,7 @@ $.extend(Selectize.prototype, {
 		}
 
 		// allow the callback to abort
-		if (!values.length || (typeof self.settings.onDelete === 'function' && self.settings.onDelete(values) === false)) {
+		if (!values.length || (typeof self.settings.onDelete === 'function' && self.settings.onDelete.apply(self, [values]) === false)) {
 			return false;
 		}
 
