10c5d57dbda4486788c0217ad76230f134415aca
Brian Reavis <brian@thirdroute.com>
2013-09-08 14:05:05 -0700

    Fixed typo in earlier commit.

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -2497,7 +2497,7 @@
 				if (options.append) {
 					var render_item = self.settings.render.item;
 					self.settings.render.item = function(data) {
-						return append(render_item.apply(this, arguments), options.html);
+						return append(render_item.apply(this, arguments), html);
 					};
 				}
 	
--- src/plugins/remove_button/plugin.js
+++ src/plugins/remove_button/plugin.js
@@ -46,7 +46,7 @@ Selectize.define('remove_button', function(options) {
 			if (options.append) {
 				var render_item = self.settings.render.item;
 				self.settings.render.item = function(data) {
-					return append(render_item.apply(this, arguments), options.html);
+					return append(render_item.apply(this, arguments), html);
 				};
 			}
 
