5131a170877c3f4fef0aef75098d8c8c6b374667
Brian Reavis <brian@thirdroute.com>
2013-10-11 23:03:13 -0700

    Disable browser autocomplete.

--- src/selectize.js
+++ src/selectize.js
@@ -110,7 +110,7 @@ $.extend(Selectize.prototype, {
 
 		$wrapper          = $('<div>').addClass(settings.wrapperClass).addClass(classes).addClass(inputMode);
 		$control          = $('<div>').addClass(settings.inputClass).addClass('items').appendTo($wrapper);
-		$control_input    = $('<input type="text">').appendTo($control).attr('tabindex', tab_index);
+		$control_input    = $('<input type="text" autocomplete="off">').appendTo($control).attr('tabindex', tab_index);
 		$dropdown_parent  = $(settings.dropdownParent || $wrapper);
 		$dropdown         = $('<div>').addClass(settings.dropdownClass).addClass(classes).addClass(inputMode).hide().appendTo($dropdown_parent);
 		$dropdown_content = $('<div>').addClass(settings.dropdownContentClass).appendTo($dropdown);
