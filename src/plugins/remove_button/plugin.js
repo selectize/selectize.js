500a7d6138b2865a9ab596d11000250b6ed48a2a
Simon Hewitt <si@sjhewitt.co.uk>
2014-02-06 16:55:08 -0800

    remove_button: fix event handler when label contains html

--- src/plugins/remove_button/plugin.js
+++ src/plugins/remove_button/plugin.js
@@ -57,7 +57,7 @@ Selectize.define('remove_button', function(options) {
 				e.preventDefault();
 				if (self.isLocked) return;
 
-				var $item = $(e.target).parent();
+				var $item = $(e.currentTarget).parent();
 				self.setActiveItem($item);
 				if (self.deleteSelection()) {
 					self.setCaret(self.items.length);
