5263ebb4ad6bf337fb946b1a727d86a418ffc2cf
Jonathan Kim <jkimbo@gmail.com>
2014-03-14 16:59:27 +0000

    Built library

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -20,6 +20,8 @@
 (function(root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		define(['jquery','sifter','microplugin'], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require('jquery'), require('sifter'), require('microplugin'));
 	} else {
 		root.Selectize = factory(root.jQuery, root.Sifter, root.MicroPlugin);
 	}
@@ -105,8 +107,10 @@
 	var KEY_ESC       = 27;
 	var KEY_LEFT      = 37;
 	var KEY_UP        = 38;
+	var KEY_P         = 80;
 	var KEY_RIGHT     = 39;
 	var KEY_DOWN      = 40;
+	var KEY_N         = 78;
 	var KEY_BACKSPACE = 8;
 	var KEY_DELETE    = 46;
 	var KEY_SHIFT     = 16;
@@ -116,6 +120,7 @@
 	
 	var TAG_SELECT    = 1;
 	var TAG_INPUT     = 2;
+	
 	
 	var isset = function(object) {
 		return typeof object !== 'undefined';
@@ -363,6 +368,10 @@
 	 * @returns {int}
 	 */
 	var measureString = function(str, $parent) {
+		if (!str) {
+			return 0;
+		}
+		
 		var $test = $('<test>').css({
 			position: 'absolute',
 			top: -99999,
@@ -396,6 +405,8 @@
 	 * @param {object} $input
 	 */
 	var autoGrow = function($input) {
+		var currentWidth = null;
+		
 		var update = function(e) {
 			var value, keyCode, printable, placeholder, width;
 			var shift, character, selection;
@@ -438,7 +449,8 @@
 			}
 	
 			width = measureString(value, $input) + 4;
-			if (width !== $input.width()) {
+			if (width !== currentWidth) {
+				currentWidth = width;
 				$input.width(width);
 				$input.triggerHandler('resize');
 			}
@@ -560,7 +572,7 @@
 	
 			$wrapper          = $('<div>').addClass(settings.wrapperClass).addClass(classes).addClass(inputMode);
 			$control          = $('<div>').addClass(settings.inputClass).addClass('items').appendTo($wrapper);
-			$control_input    = $('<input type="text" autocomplete="off">').appendTo($control).attr('tabindex', tab_index);
+			$control_input    = $('<input type="text" autocomplete="off" />').appendTo($control).attr('tabindex', tab_index);
 			$dropdown_parent  = $(settings.dropdownParent || $wrapper);
 			$dropdown         = $('<div>').addClass(settings.dropdownClass).addClass(classes).addClass(inputMode).hide().appendTo($dropdown_parent);
 			$dropdown_content = $('<div>').addClass(settings.dropdownContentClass).appendTo($dropdown);
@@ -680,7 +692,7 @@
 			self.trigger('initialize');
 	
 			// preload options
-			if (settings.preload) {
+			if (settings.preload === true) {
 				self.onSearchChange('');
 			}
 		},
@@ -846,6 +858,8 @@
 				case KEY_ESC:
 					self.close();
 					return;
+				case KEY_N:
+					if (!e.ctrlKey) break;
 				case KEY_DOWN:
 					if (!self.isOpen && self.hasOptions) {
 						self.open();
@@ -856,6 +870,8 @@
 					}
 					e.preventDefault();
 					return;
+				case KEY_P:
+					if (!e.ctrlKey) break;
 				case KEY_UP:
 					if (self.$activeOption) {
 						self.ignoreHover = true;
@@ -877,6 +893,9 @@
 					self.advanceSelection(1, e);
 					return;
 				case KEY_TAB:
+					if (self.isOpen && self.$activeOption) {
+						self.onOptionSelect({currentTarget: self.$activeOption});
+					}
 					if (self.settings.create && self.createItem()) {
 						e.preventDefault();
 					}
@@ -970,7 +989,7 @@
 			if (self.ignoreFocus) return;
 	
 			if (self.settings.create && self.settings.createOnBlur) {
-				self.createItem();
+				self.createItem(false);
 			}
 	
 			self.close();
@@ -1771,12 +1790,16 @@
 		 *
 		 * @return {boolean}
 		 */
-		createItem: function() {
+		createItem: function(triggerDropdown) {
 			var self  = this;
 			var input = $.trim(self.$control_input.val() || '');
 			var caret = self.caretPos;
 			if (!input.length) return false;
 			self.lock();
+	
+			if (typeof triggerDropdown === 'undefined') {
+				triggerDropdown = true;
+			}
 	
 			var setup = (typeof self.settings.create === 'function') ? this.settings.create : function(input) {
 				var data = {};
@@ -1796,7 +1819,7 @@
 				self.addOption(data);
 				self.setCaret(caret);
 				self.addItem(value);
-				self.refreshOptions(self.settings.mode !== 'single');
+				self.refreshOptions(triggerDropdown && self.settings.mode !== 'single');
 			});
 	
 			var output = setup.apply(this, [input, create]);
@@ -2704,7 +2727,7 @@
 					e.preventDefault();
 					if (self.isLocked) return;
 	
-					var $item = $(e.target).parent();
+					var $item = $(e.currentTarget).parent();
 					self.setActiveItem($item);
 					if (self.deleteSelection()) {
 						self.setCaret(self.items.length);
