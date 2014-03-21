cccf4627a0e23ab2218c53517c5f10887cc0b449
Brian Reavis <brian@thirdroute.com>
2014-03-20 21:47:39 -0700

    Manual merge of #330 from @jkimbo w/o built files.

--- src/.wrapper.js
+++ src/.wrapper.js
@@ -20,6 +20,8 @@
 (function(root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		define(['jquery','sifter','microplugin'], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require('jquery'), require('sifter'), require('microplugin'));
 	} else {
 		root.Selectize = factory(root.jQuery, root.Sifter, root.MicroPlugin);
 	}
