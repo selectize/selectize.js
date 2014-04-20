d13e04ebc8e77aae2cb23857975eb4f292ee2508
Will <machineboy2045@gmail.com>
2014-04-19 18:59:11 -0600

    Prevent Selectize from causing unwanted scrolling
    In Chromium, if selectize is inside of a scrollable area, moving the menu 1000px can cause the user's view to scroll 1000 pixels.

--- src/selectize.js
+++ src/selectize.js
@@ -788,7 +788,7 @@ $.extend(Selectize.prototype, {
 		var self = this;
 
 		self.setTextboxValue('');
-		self.$control_input.css({opacity: 0, position: 'absolute', left: self.rtl ? 10000 : -10000});
+		self.$control_input.css({opacity: 0, position: 'absolute'});
 		self.isInputHidden = true;
 	},
 
