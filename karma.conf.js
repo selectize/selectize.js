704476323887cc5cb5a35e4f16fde9665342625f
Brian Reavis <brian@thirdroute.com>
2015-01-29 03:29:30 -0700

    Updated karma configuration.

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,39 @@
 module.exports = function(config) {
 	var saucelabs_browsers = {
-		sl_chrome: {base: 'SauceLabs', browserName: 'chrome', platform: 'Windows 7', version: '35'},
-		sl_firefox: {base: 'SauceLabs', browserName: 'firefox', version: '30'},
-		sl_ios_safari: {base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.9', version: '7.1'},
-		sl_ie_11: {base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 8.1', version: '11'}
+		'SL_Chrome': {
+			base: 'SauceLabs',
+			browserName: 'chrome',
+			version: '39'
+		},
+		'SL_Firefox': {
+			base: 'SauceLabs',
+			browserName: 'firefox',
+			version: '31'
+		},
+		'SL_Safari': {
+			base: 'SauceLabs',
+			browserName: 'safari',
+			platform: 'OS X 10.10',
+			version: '8'
+		},
+		'SL_IE_9': {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 2008',
+			version: '9'
+		},
+		'SL_IE_10': {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 2012',
+			version: '10'
+		},
+		'SL_IE_11': {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 8.1',
+			version: '11'
+		}
 	};
 
 	var targets = {
@@ -34,18 +64,25 @@ module.exports = function(config) {
 			dir: 'coverage/'
 		},
 		sauceLabs: {
-			username: process.env.SAUCE_USERNAME,
-			accessKey: process.env.SAUCE_ACCESS_KEY,
+			startConnect: true,
+			tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
 			build: process.env.TRAVIS_BUILD_NUMBER,
 			testName: process.env.COMMIT_MESSAGE,
-			tags: ['selectize', 'test']
+			tags: ['selectize', 'test'],
+			options: {
+				'selenium-version': '2.41.0'
+			}
 		},
 		customLaunchers: saucelabs_browsers,
 		reporters: ['mocha', 'coverage'],
 		port: 8888,
 		colors: true,
+		captureTimeout: 0,
 		logLevel: config.LOG_INFO,
 		browsers: targets[process.env.TARGET || 'phantomjs'],
+		browserDisconnectTolerance: 2,
+		browserDisconnectTimeout: 10000,
+		browserNoActivityTimeout: 120000,
 		singleRun: true
 	});
 };
