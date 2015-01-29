b0c6ec50b71e0b392f1283fe02e229bb56cc78bb
Brian Reavis <brian@thirdroute.com>
2015-01-29 03:57:34 -0700

    Coverage reporting + restored original browser matrix + other fixes.

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,52 @@
 module.exports = function(config) {
-	var saucelabs_browsers = {
-		'SL_Chrome': {
-			base: 'SauceLabs',
-			browserName: 'chrome',
-			version: '39'
-		},
-		'SL_Firefox': {
-			base: 'SauceLabs',
-			browserName: 'firefox',
-			version: '31'
-		},
-		'SL_Safari': {
-			base: 'SauceLabs',
-			browserName: 'safari',
-			platform: 'OS X 10.10',
-			version: '8'
-		},
-		'SL_IE_9': {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			platform: 'Windows 2008',
-			version: '9'
-		},
-		'SL_IE_10': {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			platform: 'Windows 2012',
-			version: '10'
-		},
-		'SL_IE_11': {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			platform: 'Windows 8.1',
-			version: '11'
-		}
-	};
+	var saucelabsBrowsers = [
+		// mobile
+		{platform: 'OS X 10.10', browserName: 'iPhone', version: '8.1'},
+		{platform: 'OS X 10.10 ', browserName: 'iPhone', version: '6.0'},
+		{platform: 'OS X 10.10', browserName: 'iPad', version: '8.1'},
+		{platform: 'OS X 10.10', browserName: 'iPad', version: '6.0'},
+		{platform: 'Linux', browserName: 'android', version: '4.4'},
+		{platform: 'Linux', browserName: 'android', version: '4.3'},
+		// desktop (safari)
+		{platform: 'OS X 10.8', browserName: 'safari', version: 6},
+		{platform: 'OS X 10.9', browserName: 'safari', version: 7},
+		{platform: 'OS X 10.10', browserName: 'safari', version: 8},
+		// desktop (chrome)
+		{platform: 'OS X 10.10', browserName: 'chrome', version: 39},
+		{platform: 'OS X 10.10', browserName: 'chrome', version: 38},
+		{platform: 'OS X 10.10', browserName: 'chrome', version: 37},
+		{platform: 'Windows 7', browserName: 'chrome', version: 39},
+		{platform: 'Windows 7', browserName: 'chrome', version: 38},
+		{platform: 'Windows 7', browserName: 'chrome', version: 37},
+		// desktop (firefox)
+		{platform: 'Windows 7', browserName: 'firefox', version: 35},
+		{platform: 'Windows 8', browserName: 'firefox', version: 35},
+		{platform: 'OS X 10.10', browserName: 'firefox', version: 34},
+		{platform: 'OS X 10.10', browserName: 'firefox', version: 33},
+		{platform: 'OS X 10.10', browserName: 'firefox', version: 32},
+		// desktop (internet explorer)
+		{platform: 'Windows 8', browserName: 'iexplore', version: 10},
+		{platform: 'Windows 8.1', browserName: 'iexplore', version: 11},
+		{platform: 'Windows 7', browserName: 'iexplore', version: 9}
+	];
+
+	var customLaunchers = {};
+	saucelabsBrowsers.forEach(function(browser, i) {
+		browser.base = 'SauceLabs';
+		customLaunchers['sl' + i] = browser;
+	});
 
 	var targets = {
-		'saucelabs': Object.keys(saucelabs_browsers),
+		'saucelabs': Object.keys(customLaunchers),
 		'phantomjs': ['PhantomJS']
 	};
+
+	var reporters = ['mocha'];
+	if (process.env.TRAVIS_CI) {
+		reporters = process.env.TARGET === 'saucelabs'
+			? ['progress', 'saucelabs']
+			: ['mocha', 'coverage', 'coveralls']
+	}
 
 	config.set({
 		frameworks: ['mocha', 'chai'],
@@ -60,7 +67,7 @@ module.exports = function(config) {
 			'src/**/*.js': ['coverage']
 		},
 		coverageReporter: {
-			type: 'text-summary',
+			type: process.env.TRAVIS_CI ? 'lcov' : 'text-summary',
 			dir: 'coverage/'
 		},
 		sauceLabs: {
@@ -68,13 +75,10 @@ module.exports = function(config) {
 			tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
 			build: process.env.TRAVIS_BUILD_NUMBER,
 			testName: process.env.COMMIT_MESSAGE,
-			tags: ['selectize', 'test'],
-			options: {
-				'selenium-version': '2.41.0'
-			}
+			tags: ['selectize', 'test']
 		},
-		customLaunchers: saucelabs_browsers,
-		reporters: ['mocha', 'coverage'],
+		customLaunchers: customLaunchers,
+		reporters: reporters,
 		port: 8888,
 		colors: true,
 		captureTimeout: 0,
