d70aaa914db41475ceead8628b70f844a1e1d8c4
Kris Salvador <kris@fiscalnote.com>
2015-11-10 13:50:33 -0500

    Created click_to_edit plugin and example

---
+++ src/plugins/click_to_edit/plugin.js
@@ -0,0 +1,54 @@
+/**
+ * Plugin: "click_edit" (selectize.js)
+ * Copyright (c) 2013 Kris Salvador & contributors
+ *
+ * -- Description
+ * - Given an input selectize, users now have the ability to
+ * - click on a given item and edit them within the input.
+ *
+ * example gif: http://g.recordit.co/q391ZyVBVS.gif
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * @author Kris Salvador <krissalvador27@gmail.com>
+ */
+
+Selectize.define('click_to_edit', function( options ) {
+ var self = this;
+
+ options.text = options.text || function(option) {
+   return option[this.settings.labelField];
+ };
+
+ this.onClick = (function(e) {
+   var original = self.onClick;
+   
+   return function(e) {
+     var index, option, key;
+
+     if (e.toElement.className === 'item active') {
+       index = this.caretPos - 1;
+       if (index >= 0 && index < this.items.length) {
+         key = $(e.toElement).data('value');
+         option = this.options[key];
+
+         if (this.deleteSelection(e)) {
+           this.setTextboxValue(options.text.apply(this, [option]));
+           this.refreshOptions(true);
+         }
+         
+         e.preventDefault();
+         return;
+       }
+     }
+   };
+ })();
+
+ });
