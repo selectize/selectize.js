fcf896f24026e73bede7df3b3d6308de3de80804
Bryce Wilson <bwilson-ux@users.noreply.github.com>
2016-10-03 06:42:32 +0100

    src update for lockOptgroupOrder setting
    Same update applied to the dist version.

--- src/selectize.js
+++ src/selectize.js
@@ -1094,8 +1094,8 @@ $.extend(Selectize.prototype, {
 		// sort optgroups
 		if (this.settings.lockOptgroupOrder) {
 			groups_order.sort(function(a, b) {
-				var a_order = self.optgroups[a].$order || 0;
-				var b_order = self.optgroups[b].$order || 0;
+				var a_order = a ? self.optgroups[a].$order : 0;
+				var b_order = b ? self.optgroups[b].$order : 0;
 				return a_order - b_order;
 			});
 		}
