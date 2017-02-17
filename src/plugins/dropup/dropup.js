17763aba1797bf603a301d0e112cd899641fd235
DavidHequet <onclebob@gmail.com>
2017-02-17 14:23:31 +0100

    Create dropup.js

---
+++ src/plugins/dropup/dropup.js
@@ -0,0 +1,13 @@
+Selectize.define('dropup', function (options) {
+    var self = this;
+    this.positionDropdown = function () {
+        var $control = this.$control;
+        var offset = this.settings.dropdownParent === 'body' ? $control.offset() : $control.position();
+        offset.top -= this.$dropdown.outerHeight(true);
+        this.$dropdown.css({
+            width: $control.outerWidth(),
+            top: offset.top,
+            left: offset.left
+        });
+    }
+});
