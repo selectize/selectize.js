b7ef81229858871e149dc3913b2f23c15bb1e1e3
Steven Bone <steven@stevenbone.com>
2017-05-05 13:24:43 -0500

    Add plugin to accomodate existing Drip markup

---
+++ src/plugins/drop_option_template/plugin.js
@@ -0,0 +1,46 @@
+/**
+ * Plugin: "drip_option_template" (selectize.js)
+ *
+ * Handles Drip's previous <select> markup where
+ * <option>s that had a secondary line in their
+ * UI would be rendered with `data-description`
+ * attributes.
+ *
+ * This plugin checks for `data-description` on
+ * <option>s and replaces the default 'option'
+ * template with one that shows the description text
+ *
+ */
+
+
+Selectize.define('drip_option_template', function(options) {
+  var self = this;
+  var original = self.setupTemplates;
+
+  // if source <select>'s <option>s have `data-description`
+  // attrs, extract and append to Selectize's `options`
+  var selectOptions = self.$input[0].options;
+
+  for (var i = 0; i < selectOptions.length; i++) {
+    if (selectOptions[i].dataset.hasOwnProperty('description')) {
+      this.options[selectOptions[i].value]['description'] = selectOptions[i].dataset.description;
+    }
+  }
+
+  // add custom template to available option templates
+  this.setupTemplates = (function() {
+    var templates = {
+      'option': function(data, escape) {
+        return ('<div class="option">' +
+          escape(data[self.settings.labelField]) +
+          (data.description ? '<div class="desc">' + data.description + '</div>' : '') +
+          '</div>');
+      }
+    };
+
+    self.settings.render = $.extend({}, templates, self.settings.render);
+    original.apply(self, arguments);
+  });
+
+});
+
