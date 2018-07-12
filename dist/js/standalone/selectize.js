eb0fca364f9bd6864ae197ea58c853f2717052a9
Jonathan Allard <jonathan@allard.io>
2018-07-12 18:29:54 -0300

    Released 0.12.6.

--- dist/js/standalone/selectize.js
+++ dist/js/standalone/selectize.js
@@ -635,7 +635,7 @@
 }));
 
 /**
- * selectize.js (v0.12.5)
+ * selectize.js (v0.12.6)
  * Copyright (c) 2013–2015 Brian Reavis & contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
