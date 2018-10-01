b370c8a65441911ee99f72b1cfbc2de5ebaaa4aa
Stefan Crnjaković <stefan@hotmail.rs>
2018-10-01 14:09:00 +0200

    Fixes persistant .selected class
    Removes .selected class from options that are not selected any more

--- src/selectize.js
+++ src/selectize.js
@@ -1142,6 +1142,9 @@ $.extend(Selectize.prototype, {
 
 		// add "selected" class to selected options
 		if (!self.settings.hideSelected) {
+			// clear selection on all previously selected elements first
+			self.$dropdown.find('.selected').removeClass('selected');
+
 			for (i = 0, n = self.items.length; i < n; i++) {
 				self.getOption(self.items[i]).addClass('selected');
 			}
