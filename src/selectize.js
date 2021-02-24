76a8a6d2ce39546732dbcdbf71f515830baa7b91
Anand Phulwani <portalcontrol@harlax.com>
2021-02-24 09:32:09 +0530

    Update backspace action on dropdown.

--- src/selectize.js
+++ src/selectize.js
@@ -1437,6 +1437,17 @@ $.extend(Selectize.prototype, {
 	},
 
 	/**
+	 * Returns the jQuery element of the first
+	 * selectable option.
+	 *
+	 * @return {object}
+	 */
+	getFirstOption: function() {
+		var $options = this.$dropdown.find('[data-selectable]');
+		return $options.length > 0 ? $options.eq(0) : $();
+	},
+
+	/**
 	 * Returns the jQuery element of the next or
 	 * previous selectable option.
 	 *
@@ -1956,7 +1967,7 @@ $.extend(Selectize.prototype, {
 		selection = getSelection(self.$control_input[0]);
 
 		if (self.$activeOption && !self.settings.hideSelected) {
-			option_select = self.getAdjacentOption(self.$activeOption, -1).attr('data-value');
+			option_select = self.getFirstOption().attr('data-value');
 		}
 
 		// determine items that will be removed
