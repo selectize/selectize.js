542d6054662cb43d2f778f68a41503da7a0bb495
Ris Adams <chris@tuesdaybegins.com>
2022-07-09 10:07:50 -0400

    fix: 1827

--- src/selectize.js
+++ src/selectize.js
@@ -316,7 +316,7 @@ $.extend(Selectize.prototype, {
 				return '<div class="item">' + escape(data[field_label]) + '</div>';
 			},
 			'option_create': function(data, escape) {
-				return '<div class="create">Add <strong>' + escape(data.input) + '</strong>&hellip;</div>';
+				return '<div class="create">Add <strong>' + escape(data.input) + '</strong>&#x2026;</div>';
 			}
 		};
 
