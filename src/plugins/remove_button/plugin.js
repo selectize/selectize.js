a63c4494c9dd0a656b8d8cea2295aabe85180c3c
WINKLER Fabien <fabien.winkler@outlook.fr>
2022-10-06 14:28:18 +0200

    Fix: remove 'singleClose' from 'remove_plugin' assumed now by 'clear_button'

--- src/plugins/remove_button/plugin.js
+++ src/plugins/remove_button/plugin.js
@@ -23,53 +23,6 @@ Selectize.define('remove_button', function(options) {
 			append    : true
 		}, options);
 
-		var singleClose = function(thisRef, options) {
-
-			options.className = 'remove-single';
-
-			var self = thisRef;
-			var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
-
-			/**
-			 * Appends an element as a child (with raw HTML).
-			 *
-			 * @param {string} html_container
-			 * @param {string} html_element
-			 * @return {string}
-			 */
-			var append = function(html_container, html_element) {
-				return $('<span>').append(html_container)
-					.append(html_element);
-			};
-
-			thisRef.setup = (function() {
-				var original = self.setup;
-				return function() {
-					// override the item rendering method to add the button to each
-					if (options.append) {
-						var id = $(self.$input.context).attr('id');
-						var selectizer = $('#'+id);
-
-						var render_item = self.settings.render.item;
-						self.settings.render.item = function(data) {
-							return append(render_item.apply(thisRef, arguments), html);
-						};
-					}
-
-					original.apply(thisRef, arguments);
-
-					// add event listener
-					thisRef.$control.on('click', '.' + options.className, function(e) {
-						e.preventDefault();
-						if (self.isLocked) return;
-
-						self.clear();
-					});
-
-				};
-			})();
-		};
-
 		var multiClose = function(thisRef, options) {
 
 			var self = thisRef;
@@ -117,10 +70,5 @@ Selectize.define('remove_button', function(options) {
 			})();
 		};
 
-		if (this.settings.mode === 'single') {
-			singleClose(this, options);
-			return;
-		} else {
-			multiClose(this, options);
-		}
+    multiClose(this, options);
 });
