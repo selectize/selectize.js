b41cca209f4fb75750316a17a28b75972727d71b
Ris Adams <chris@tuesdaybegins.com>
2022-11-06 13:09:56 -0500

    made plugins load in order

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const concat = require('gulp-concat');
 const dartSass = require('sass');
+const del = require('del');
+const fs = require('fs');
 const gulpSass = require('gulp-sass');
 const less = require('gulp-less');
 const path = require('path');
@@ -11,20 +13,32 @@ const uglify = require('gulp-uglify');
 const uglifycss = require('gulp-uglifycss');
 const wrapper = require('@risadams/gulp-wrapper');
 
-const del = require('del');
 const { src, dest, series, watch } = require('gulp');
-
-// Tasks
 
 const cleanLibs = async () => del('dist/**/*');
 const loadDependencies = async () => await _copyLibs();
 const copyDependencies = async () => src(['lib/**/*']).pipe(dest('dist/lib'));
 const copySrc = async () => {
+
+  let scripts = [
+    'src/contrib/*.js',
+    'src/constants.js',
+    'src/utils.js',
+    'src/selectize.js',
+    'src/defaults.js',
+    'src/selectize.jquery.js'
+  ];
+
+  // Add in all plugin scripts in a predictable order
+  fs.readdirSync('src/plugins').sort().forEach((file) => {
+    scripts.push(`src/plugins/${file}/plugin.js`);
+  });
+
   setTimeout(async () => {
     await _compileLess();
     await _compileSass();
-    await _compileJavascript();
-    setTimeout(async () => { await _minifyScripts(); }, 1000);
+    await _compileJavascript(scripts);
+    await _minifyScripts(scripts);
   }, 1000);
 };
 const watchFiles = async () => watch(['src/**/*.{js,css,less,scss}']).on('change', series(loadDependencies, copyDependencies, copySrc));
@@ -137,16 +151,8 @@ const _compileSass = async () => {
   }
 }
 
-const _compileJavascript = async () =>
-  src([
-    'src/contrib/*.js',
-    'src/constants.js',
-    'src/utils.js',
-    'src/selectize.js',
-    'src/defaults.js',
-    'src/selectize.jquery.js',
-    'src/plugins/**/*.js',
-  ])
+const _compileJavascript = async (scripts) =>
+  src(scripts)
     .pipe(concat('selectize.js'))
     .pipe(wrapper({
       header: amd_header,
@@ -157,16 +163,8 @@ const _compileJavascript = async () =>
     .pipe(replace(/@@version/g, getVersion()))
     .pipe(dest('dist/js'));
 
-const _minifyScripts = async () =>
-  src([
-    'src/contrib/*.js',
-    'src/constants.js',
-    'src/utils.js',
-    'src/selectize.js',
-    'src/defaults.js',
-    'src/selectize.jquery.js',
-    'src/plugins/**/*.js',
-  ])
+const _minifyScripts = async (scripts) =>
+  src(scripts)
     .pipe(concat('selectize.min.js'))
     .pipe(wrapper({
       header: amd_header,
