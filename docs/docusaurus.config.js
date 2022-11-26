4a3688afcd8ef6e4db1027ad2a648ee469263ad8
Ris Adams <chris@tuesdaybegins.com>
2022-11-26 11:29:24 -0500

    Linting and deployment prep

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,6 +18,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'selectize', // Usually your GitHub org/user name.
   projectName: 'selectize', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // Branch that GitHub pages will deploy from.
+  trailingSlash: false, // GitHub pages doesn't support trailing slashes.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
