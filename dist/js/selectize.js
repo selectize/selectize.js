0ceeb35e6901ba3fc597e7dd27a198d591e02b49
Vladimir Bikmeev <elmakan@gmail.com>
2023-10-26 11:39:41 +0600

    Add plugin to allow deselect options via dropdown (#4)
    * Stash changes
    
    * Fixed plugin
    
    * Added styles
    
    * Reused default color
    
    * Removed style

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -3125,6 +3125,34 @@ Selectize.define("clear_button", function (options) {
   })();
 });
 
+Selectize.define('deselect_options_via_dropdown', function () {
+	var option, self = this;
+
+	function onOptionDeselect(e) {
+		option = e.currentTarget;
+		option.classList.remove('selected');
+		self.removeItem(option.dataset.value)
+	}
+
+	this.setup = (function () {
+		var original = self.setup;
+		return function () {
+			this.settings.hideSelected = false;
+			original.apply(this, arguments);
+
+			self.$dropdown.off('mouseup click', '[data-selectable]');
+
+			self.$dropdown.on('click', '[data-selectable]', function (e) {
+				if (e.currentTarget.classList.contains('selected')) {
+					onOptionDeselect(e);
+				} else {
+					self.onOptionSelect(e);
+				}
+			});
+		};
+	})();
+});
+
 Selectize.define('drag_drop', function(options) {
 	if (!$.fn.sortable) throw new Error('The "drag_drop" plugin requires jQuery UI "sortable".');
 	if (this.settings.mode !== 'multi') return;
