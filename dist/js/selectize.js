03903e388dbf9f1ff6b5b912b097c615f5da1252
Vladimir Bikmeev <elmakan@gmail.com>
2023-11-15 12:01:18 +0600

    Allow to deselect via enter button (#7)
    * Applied to deselect via enter button
    
    * Removed unnecessary argument

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -3128,28 +3128,27 @@ Selectize.define("clear_button", function (options) {
 Selectize.define('deselect_options_via_dropdown', function () {
 	var option, self = this;
 
-	function onOptionDeselect(e) {
-		option = e.currentTarget;
-		option.classList.remove('selected');
-		self.removeItem(option.dataset.value)
-	}
-
 	this.setup = (function () {
 		var original = self.setup;
 		return function () {
 			this.settings.hideSelected = false;
 			original.apply(this, arguments);
 
-			self.$dropdown.off('mouseup click', '[data-selectable]');
-
-			self.$dropdown.on('click', '[data-selectable]', function (e) {
-				if (e.currentTarget.classList.contains('selected')) {
-					onOptionDeselect(e);
-				} else {
-					self.onOptionSelect(e);
-				}
-			});
+			self.$dropdown.off('mouseup', '[data-selectable]');
 		};
+	})();
+
+	this.onOptionSelect = (function () {
+		var original = self.onOptionSelect;
+		return function () {
+			option = arguments[0].currentTarget[0] || arguments[0].currentTarget;
+			if (option.classList.contains('selected')) {
+				option.classList.remove('selected');
+				self.removeItem(option.dataset.value)
+			} else {
+				original.apply(this, arguments);
+			}
+		}
 	})();
 });
 
